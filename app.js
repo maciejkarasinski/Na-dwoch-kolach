f0796d6b3d83f4f7e9196e326bea8e8b0abc46e5
Timtho <maciej.karasinski.95@gmail.com>
2020-04-12 18:50:42 +0200

    filled some html

--- app.js
+++ app.js
@@ -13,7 +13,45 @@ router.get('/dlaczego-rower', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/dlaczego.html'));
 });
 
+router.get('/rodzaje-rowerow', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/rodzaje.html'));
+});
+
+router.get('/rower-szosowy', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/szosowy.html'));
+});
+
+router.get('/rower-gorski', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/gorski.html'));
+});
+
+router.get('/rower-miejski', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/miejski.html'));
+});
+
+router.get('/akcesoria-rowerowe', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/akcesoria.html'));
+});
+
+router.get('/kask-rowerowy', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/kask.html'));
+});
+
+router.get('/pompka-do-roweru', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/pompka.html'));
+});
+
+router.get('/lampka-do-roweru', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/lampka.html'));
+});
+
+router.get('/galeria-zdjec-rowery', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/galeria.html'));
+});
+
+router.get('/wymagania-projektu', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/wymagania.html'));
+});
+
 app.use('/', router);
 app.listen(process.env.PORT || 3000);
-
-console.log('Running');
